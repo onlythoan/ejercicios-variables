2622e7e3aecf751be7283b021ccbebe0aa0f115d
Germán Martínez <90202277+onlythoan@users.noreply.github.com>
2021-09-16 13:37:44 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+//Ejercicio 0
+
+let nombreusuario;
+let apellidousuario;
+nombreusuario = window.prompt("Ingrese su nombre");
+console.log(nombreusuario);
+alert("Hola"+" "+ nombreusuario);
+apellidousuario = window.prompt("Ingrese su apellido");
+console.log("Hola"+ " " + nombreusuario + " " + apellidousuario); 
+
+//Ejercio 1
+
+let primernumerodni;
+let ulitmonumerodni;
+let resultado;
+primernumerodni = window.prompt("Primer numero dni");
+primernumerodni = parseInt(primernumerodni) 
+ulitmonumerodni = window.prompt("Segundo numero dni");
+ulitmonumerodni = parseInt(ulitmonumerodni)
+resultado = primernumerodni + ulitmonumerodni;
+console.log("El resultado es:" + resultado);
+
+// Ejercicio 2
+
+
